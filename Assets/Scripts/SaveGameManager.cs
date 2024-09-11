d2b772a996c229303bdbb0c66de860ac2984c538
Solus-dot <solus23451@gmail.com>
2024-09-12 04:33:18 +0530

    Added Basic Main Menu and TMP Stuff

---
+++ Assets/Scripts/SaveGameManager.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SaveGameManager : MonoBehaviour
+{
+    public static SaveGameManager instance;
+    [SerializeField] int worldSceneIndex = 1;
+
+    private void Awake() {
+        if (instance == null) {
+            instance = this;
+        } else {
+            Destroy(gameObject);
+        }
+    }
+
+    private void Start() {
+        DontDestroyOnLoad(gameObject);
+    }
+
+    public IEnumerator LoadNewGame() {
+        AsyncOperation loadOperation = SceneManager.LoadSceneAsync(worldSceneIndex);
+        yield return null;
+    }
+}
---
+++ Assets/Scripts/TitleScreenManager.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Netcode;
+
+public class TitleScreenManager : MonoBehaviour
+{
+    public void StartNetworkAsHost() {
+        NetworkManager.Singleton.StartHost();
+    }
+
+    public void StartNewGame() {
+        StartCoroutine(SaveGameManager.instance.LoadNewGame());
+    }
+}
